91944814ba69c91855ace8bfa03b91fd825a0032
Andrew <aschwa32@gmail.com>
2014-06-22 15:05:10 -0700

    committing files memoization included

--- Stakeout.js
+++ Stakeout.js
@@ -1,11 +1,20 @@
+var housesOptimal = new Array();
 var planner = function(houses){
-	if(houses.length==0) return 0;
 	for(var a in houses){
 		if (houses[a] < 0) return "Negative numbers are not allowed";
+		if (!(houses[a] === parseInt(houses[a]))) return "All values must be integers";
+		housesOptimal[a] = -1;
 	}
+	return plannerRec(houses, 0);
+}
+function plannerRec(houses, houseIndex){
+	if(houses.length==0) return 0;
 	if(houses.length==1) return houses[0];
-	var takeItSet = houses[0] + planner(houses.slice(2));
-	var dontTakeItSet = planner(houses.slice(1));
-	return (takeItSet > dontTakeItSet ? takeItSet : dontTakeItSet);
+	if(housesOptimal[houseIndex]==-1){
+		var takeItSet = houses[0] + planner(houses.slice(2), houseIndex + 2);
+		var dontTakeItSet = planner(houses.slice(1), houseIndex + 1);
+		housesOptimal[houseIndex] = (takeItSet > dontTakeItSet ? takeItSet : dontTakeItSet);
+	}
+	return housesOptimal[houseIndex];
 }
 module.exports.planner = planner;
